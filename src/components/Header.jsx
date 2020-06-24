7e89785f714ded16b901564000a8f24c99b4dcb5
Utkarsh Yadav <utkarshyadav_as@srmuniv.edu.in>
2020-06-24 23:59:28 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     
         <div class="topnav">
-            <a href="#"><img  class="icon-spin" src="https://img.icons8.com/nolan/96/coronavirus.png" width="50" /></a>
+            <a><img  class="icon-spin" src="https://img.icons8.com/nolan/96/coronavirus.png" width="50" /></a>
             <a className="space" href="#">COVID-19</a>
             <div className="spacer">          
             <a className="space" href="https://www.who.int/">WHO</a>
